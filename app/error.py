733fc6e6eba28e9d8cc901fb59d93fc5327acb75
Gregory B. Poole <gbpoole@gmail.com>
2022-08-24 06:28:07 +0000

    Logging working and commands now confirmed to be received correctly from Slack workspace.

--- app/error.py
+++
@@ -1,8 +0,0 @@
-import logging
-from slackers.hooks import commands
-
-log = logging.getLogger(__name__)
-
-@commands.on('error')
-def log_error(exc):
-    log.error(str(exc))
--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@
 
 from fastapi import FastAPI, Depends
 from app.config import get_settings, Settings
-from slackers.server import router
+from slackers.server import router, commands
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+log = logging.getLogger(__name__)
 
 app = FastAPI()
 app.include_router(router, prefix='/slack')
-
 
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
@@ -16,3 +18,12 @@ async def pong(settings: Settings = Depends(get_settings)):
         "environment": settings.environment,
         "testing": settings.testing
     }
+
+@commands.on("time")  # responds to "/time"  
+async def handle_command(payload):
+    pass
+
+@commands.on('error')
+def log_error(exc):
+    log.error(str(exc))
+
