c2d65dc8ad93e74a18ca5b0bcb3c3bf4355a8d74
Gregory Poole <gbpoole@gmail.com>
2022-08-30 21:27:41 +1000

    Coda rows can be written now.

--- app/coda.py
+++ app/coda.py
@@ -8,15 +8,13 @@ log = logging.getLogger(__name__)
 settings = config.Settings()
 coda = config.CodaClient(settings)
 
-# TODO: 
-#  1) Query on tables
-#  2) Write new rows
+query = '"Name":"Greg"'
+for row in coda.get_rows('people', query=query):
+    print(row)
 
-for table in coda.doc.list_tables():
-
-    print(f'{table}')
-
-    if table.name == "Projects":
-
-        for row in coda.get_rows(table):
-            print(row)
+data = [
+    {'Developer': 'Greg','Project':'OzSTAR','Duration [Days]':'5 days'},
+    #  {'Developer': 'Greg','Project':'OzSTAR','Duration [Days]':'3 days'},
+    #  {'Developer': 'Lewis','Project':'GWCloud','Duration [Days]':'1 week'},
+        ]
+coda.put_rows('timesheet',data)
--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
 import logging
 import os
 import slack_sdk as slack
-from codaio import Coda, Document
+import urllib.parse
+import json
+from codaio import Coda, Document, Cell
 import requests
 from functools import lru_cache
 from pydantic import BaseSettings, ValidationError
@@ -32,16 +34,44 @@ class CodaClient(object):
         self.doc    = Document(settings.CODA_DOC_ID, coda=self.client)
         self.settings = settings
 
-    def get_rows(self, table):
+        # Fetch the metadata for the tables that we will need
+        table_alias_list = {
+            'People':            'people',
+            'Slack Users':       'slack',
+            'Projects':          'projects',
+            'Timesheet Entries': 'timesheet',
+        }
+        self.tables = {}
+        tables = self.doc.list_tables()
+        for table in tables:
+            if table.name in table_alias_list.keys():
+                self.tables[table_alias_list[table.name]]=table
+
+    def get_rows(self, table_alias, query):
 
         results = []
-        for row in self.client.list_rows(self.doc.id, table.id, use_column_names=True)['items']:
+        for row in self.client.list_rows(self.doc.id, self.tables[table_alias].id, use_column_names=True, query=query)['items']:
             result = {}
             for key, value in row['values'].items():
                 result[key]=value
             results.append(result)
 
         return results
+
+    def put_rows(self, table_alias, data_in):
+
+        rows = []
+        for data_i in data_in:
+            row = []
+            for key, value in data_i.items():
+                row.append(Cell(column=key,value_storage=value))
+            rows.append(row)
+
+        #  print("Write:\n"+json.dumps(rows,separators=(',', ':')))
+        self.tables[table_alias].upsert_rows(rows)
+
+        return
+
 
 @lru_cache()
 def get_settings() -> BaseSettings:
